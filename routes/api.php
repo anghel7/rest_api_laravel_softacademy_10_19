a1a2cda6a3610af09e7ef05bb1169a14249a87fb
Angel Antezana <a.antezana@empresasim.com>
2019-11-28 10:34:21 -0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Symfony\Component\Routing\Route;
+//use Symfony\Component\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
